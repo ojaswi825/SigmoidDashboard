e593b8c447183363e5c8ae96bfe3395c6c9e5dd1
Ojaswi Athghara <ojaswi.athghara98@gmail.com>
2022-06-04 15:10:39 +0530

    removed data reducer

--- src/Redux/AppReducer.js
+++ src/Redux/AppReducer.js
@@ -1,8 +1,6 @@
 import { combineReducers } from "redux";
 import { authenticationReducer } from "./Authentication/AuthenticationReducer";
-import { dateRangeReducer } from "./Filters/DateRange/DateRangeReducer";
 
 export const appReducer = combineReducers({
     authentication: authenticationReducer,
-    dateRange: dateRangeReducer,
 });
--- src/Redux/Filters/DateRange/DateRangeActionTypes.js
+++
@@ -1,2 +0,0 @@
-export const SET_DEFAULT = "SET_DEFAULT";
-export const SET_DATE_RANGE = "SET_DATE_RANGE";
